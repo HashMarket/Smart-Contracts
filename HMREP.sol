583070de8aec8c5f26b51bdfcad344472779f404
jordiguirao92 <38755692+jordiguirao92@users.noreply.github.com>
2019-02-03 00:21:42 +0100

    Update HMREP.sol

--- HMREP.sol
+++ HMREP.sol
@@ -31,23 +31,84 @@ library SafeMath {
 }
 
 
-
 contract owned {
-    address public owner;
+
+    mapping(address => bool) public ownerslist;
 
     constructor() public {
-        owner = msg.sender;
+        ownerslist[msg.sender] = true;
     }
 
-    modifier onlyOwner {
-        require(msg.sender == owner);
+    event OwnerslistAddressAdded(address addr);
+    event OwnerslistAddressRemoved(address addr);
+
+
+    modifier onlyOwner() {
+        require(ownerslist[msg.sender]);
         _;
     }
 
-    function transferOwnership(address newOwner) onlyOwner public {
-        owner = newOwner;
+
+    /**
+     * @dev add an address to the ownerslist
+     * @param addr address
+     * @return true if the address was added to the ownerslist, false if the address was already in the ownerslist 
+    */
+    function addOwner(address addr) onlyOwner public returns(bool success) {
+        if (!ownerslist[addr]) {
+            ownerslist[addr] = true;
+            emit OwnerslistAddressAdded(addr);
+            success = true; 
+        }
     }
+
+
+    /**
+     * @dev add addresses to the ownerslist
+     * @param addrs addresses
+     * @return true if at least one address was added to the ownerslist, 
+     * false if all addresses were already in the ownerslist
+    */
+    function addAddressesToOwnerslist(address[] memory addrs) onlyOwner public returns(bool success) {
+        for (uint256 i = 0; i < addrs.length; i++) {
+             if (addOwner(addrs[i])) {
+                success = true;
+            }
+        }
+    }
+
+
+    /**
+      * @dev remove an address from the ownerslist
+      * @param addr address
+      * @return true if the address was removed from the ownerslist, 
+      * false if the address wasn't in the ownerslist in the first place 
+    */
+    function removeOwner(address addr) onlyOwner public returns(bool success) {
+        if (ownerslist[addr]) {
+            ownerslist[addr] = false;
+            emit OwnerslistAddressRemoved(addr);
+            success = true;
+        }
+    }
+  
+  
+   /**
+     * @dev remove addresses from the ownerslist
+     * @param addrs addresses
+     * @return true if at least one address was removed from the ownerslist, 
+     * false if all addresses weren't in the ownerslist in the first place
+   */
+    function removeAddressesFromOwnerslist(address[] memory addrs) onlyOwner public returns(bool success) {
+        for (uint256 i = 0; i < addrs.length; i++) {
+            if (removeOwner(addrs[i])) {
+            success = true;
+            }
+        }
+    }
+  
 }
+  
 
 
 interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes calldata _extraData) external; }
@@ -64,13 +125,10 @@ contract HMREPERC20 is owned {
 
     // This creates an array with all balances
     mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
     mapping (address => bool) public frozenAccount;
 
     // This generates a public event on the blockchain that will notify clients
     event Transfer(address indexed from, address indexed to, uint256 value);
-    // This generates a public event on the blockchain that will notify clients
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
     // This notifies clients about the amount burnt
     event Burn(address indexed from, uint256 value);
     //This generates a public event on the blockchain that will notify clients 
@@ -87,7 +145,6 @@ contract HMREPERC20 is owned {
     }
     
     
-    
      /// @notice Gets the balance of the specified address.
      /// @param _owner The address to query the the balance of.
      /// @return An uint256 representing the amount owned by the passed address.
@@ -95,17 +152,6 @@ contract HMREPERC20 is owned {
     return balanceOf[_owner];
     
     }
-
-
-     /// @notice Function to check the amount of tokens that an owner allowed to a spender.
-     /// @param _owner address The address which owns the funds.
-     /// @param _spender address The address which will spend the funds.
-     /// @return A uint256 specifying the amount of tokens still available for the spender.
-    function allowanceAddress(address _owner, address _spender) public view returns (uint256) {
-    return allowance[_owner][_spender];
-    
-    }
-
 
 
     /* Internal transfer, only can be called by this contract */
@@ -131,79 +177,6 @@ contract HMREPERC20 is owned {
     }
     
 
-    
-     /// @notice Transfer tokens from other addressSend `_value` tokens to `_to` in behalf of `_from`
-     /// @param _from The address of the sender
-     /// @param _to The address of the recipient
-     /// @param _value the amount to send
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_value <= allowance[_from][msg.sender]);     // Check allowance
-        allowance[_from][msg.sender] -= _value;
-        _transfer(_from, _to, _value);
-        return true;
-    }
-
-
-    
-     /// @notice Set allowance for other address: Allows `_spender` to spend no more than `_value` tokens in your behalf
-     /// @param _spender The address authorized to spend
-     /// @param _value the max amount they can spend
-    function approve(address _spender, uint256 _value) public returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-
-
-
-    /// @notice Increase the amount of tokens that an owner allowed to a spender.
-        /// approve should be called when allowed[_spender] == 0. To increment
-        /// allowed value is better to use this function to avoid 2 calls (and wait until
-        /// the first transaction is mined)
-    /// @param _spender The address which will spend the funds.
-    /// @param _addedValue The amount of tokens to increase the allowance by.
-  function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
-    allowance[msg.sender][_spender] = allowance[msg.sender][_spender].add(_addedValue);
-    emit Approval(msg.sender, _spender, allowance[msg.sender][_spender]);
-    return true;
-  }
-
-
-
-  
-   /// @notice Decrease the amount of tokens that an owner allowed to a spender.
-       /// approve should be called when allowed[_spender] == 0. To decrement
-       /// allowed value is better to use this function to avoid 2 calls (and wait until
-       /// the first transaction is mined)
-   /// @param _spender The address which will spend the funds.
-   /// @param _subtractedValue The amount of tokens to decrease the allowance by.
-  function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
-    uint oldValue = allowance[msg.sender][_spender];
-    if (_subtractedValue > oldValue) {
-      allowance[msg.sender][_spender] = 0;
-    } else {
-      allowance[msg.sender][_spender] = oldValue.sub(_subtractedValue);
-    }
-    emit Approval(msg.sender, _spender, allowance[msg.sender][_spender]);
-    return true;
-  }
-
-
-
-     /// @notice Set allowance for other address and notify: Allows `_spender` to spend no more than `_value` tokens in your behalf, and then ping the contract about it
-     /// @param _spender The address authorized to spend
-     /// @param _value the max amount they can spend
-     /// @param _extraData some extra information to send to the approved contract
-    function approveAndCall(address _spender, uint256 _value, bytes memory _extraData) public returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, address(this), _extraData);
-            return true;
-        }
-    }
-
-
 
      /// @notice Destroy tokens: Remove `_value` tokens from the system irreversibly
      /// @param _value the amount of money to burn
@@ -216,22 +189,6 @@ contract HMREPERC20 is owned {
     }
 
 
-    
-    
-     /// @notice Destroy tokens from other account: Remove `_value` tokens from the system irreversibly on behalf of `_from`.
-     /// @param _from the address of the sender
-     /// @param _value the amount of money to burn
-    function burnFrom(address _from, uint256 _value) onlyOwner public returns (bool success) {
-        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
-        require(_value <= allowance[_from][msg.sender]);    // Check allowance
-        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
-        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
-        totalSupply -= _value;                              // Update totalSupply
-        emit Burn(_from, _value);
-        return true;
-    }
-    
-    
     
      /// @notice `freeze? Prevent | Allow` `target` from sending & receiving tokens
      /// @param target Address to be frozen
@@ -247,12 +204,12 @@ contract HMREPERC20 is owned {
      /// @param target Address to receive the tokens
      /// @param mintedAmount the amount of tokens it will receive
     function mintToken(address target, uint256 mintedAmount) onlyOwner public {
+        require(!frozenAccount[target]);
         balanceOf[target] += mintedAmount;
         totalSupply += mintedAmount;
         emit Transfer(address(0), address(this), mintedAmount);
         emit Transfer(address(this), target, mintedAmount);
     }
-    
     
     
 }
